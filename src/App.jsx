68c001a3562f7e419a09f0d188574543c18c5b7d
Kunal Ojha <kunalojha47@gmail.com>
2025-08-19 15:43:38 +0530

    Best eectronics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
-import Component2 from './Component2.jsx'
+import BestElectronic from './BestElectronic.jsx'
+
 
 function App() {
   return (
     <div>
       <Header/>
-      <Component2/>
-      {/* <Footer/> */}
+      <BestElectronic/>
+      <Footer/>
 
     </div>
   )
