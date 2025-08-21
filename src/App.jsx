c561d9a5030f396a2223014aaf412f7bd32734b8
Kunal Ojha <kunalojha47@gmail.com>
2025-08-21 10:20:45 +0530

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
-import Footer from './Footer.jsx'
-import Header from './Header.jsx'
-import BestElectronic from './BestElectronic.jsx'
+// import Footer from './Footer.jsx'
+// import Header from './Header.jsx'
+// import BestElectronic from './BestElectronic.jsx'
 
 
 function App() {
   return (
     <div>
-      <Header/>
+      {/* <Header/>
       <BestElectronic/>
-      <Footer/>
+      <Footer/> */}
+
+      <div className="carousel">
+        <div className="card"><h3>card1</h3></div>
+        <div className="card"><h3>card2</h3></div>
+        <div className="card"><h3>card3</h3></div>
+        <div className="card"><h3>card4</h3></div>
+        <div className="card"><h3>card5</h3></div>
+        <div className="card"><h3>card6</h3></div>
+        <div className="card"><h3>card7</h3></div>
+        <div className="card"><h3>card8</h3></div>
+        <div className="card"><h3>card9</h3></div>
+        <div className="card"><h3>card10</h3></div>
+        <div className="card"><h3>card11</h3></div>
+        <div className="card"><h3>card12</h3></div>
+        <div className="card"><h3>card13</h3></div>
+        <div className="card"><h3>card14</h3></div>
+      </div>
 
     </div>
   )
